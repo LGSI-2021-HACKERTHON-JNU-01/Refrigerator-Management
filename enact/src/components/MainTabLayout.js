6e18f80d12e7d40c8f67a8b010ba6a7a11201aad
ksg0605 <ksg0605@gmail.com>
2021-07-21 16:52:14 +0900

    refactored code

--- enact/src/components/MainTabLayout.js
+++ enact/src/components/MainTabLayout.js
@@ -1,9 +1,7 @@
 import kind from '@enact/core/kind';
 import TabLayout, { Tab } from '@enact/sandstone/TabLayout';
 import Item from '@enact/sandstone/Item'
-import { Header, Panels } from '@enact/sandstone/Panels';
 
-import PropTypes from 'prop-types';
 
 import HomePanel from '../views/HomePanel';
 import ScanPanel from '../views/ScanPanel';
@@ -12,62 +10,10 @@ import TimerPanel from '../views/TimerPanel';
 import RecipePanel from '../views/RecipePanel';
 import CalendarPanel from '../views/CalendarPanel';
 
-// const tabs = [
-//     'Home',
-//     'Scan',
-//     'List',
-//     'Timer',
-//     'Recipe',
-//     'Calendar'
-// ];
-
-// const homeIcon = <Icon>home</Icon>;
-// const scanIcon = <Icon>picture</Icon>
-// const listIcon = <Icon>list</Icon>;
-// const timerIcon = <Icon>timer</Icon>;
-// const recipeIcon = <Icon>guide</Icon>;
-// const calendarIcon = <Icon>checker</Icon>;
-
 const MainTabLayout = kind({
     name: 'MainTabLayout',
 
-    propTypes: {
-        index: PropTypes.number,
-        tab: PropTypes.number,
-        children: PropTypes.node,
-        icon: PropTypes.string,
-        onTabClick: PropTypes.func
-    },
-
-    defaultProps: {
-        index: 0,
-        tab: 0,
-        children: [
-            {HomePanel},
-            {ScanPanel},
-            {ListPanel},
-            {TimerPanel},
-            {RecipePanel},
-            {CalendarPanel}
-        ]
-    },
-
-    styles: {/* unchanged */},
-
-    handlers: {
-        onTabClick: (ev, { onTabClick }) => {
-            if (onTabClick) {
-                onTabClick({
-                    tab: ev.children
-                });
-                // children({
-                //     index: ev.tab
-                // });
-            }
-        }
-    },
-
-    render: ({children}) => (
+    render: () => (
         <TabLayout>
             <Tab title="Home" icon="home">
                 <Item>Home</Item>
--- enact/src/views/MainPanel.js
+++ enact/src/views/MainPanel.js
@@ -1,10 +1,7 @@
-// import React from 'react';
 import kind from '@enact/core/kind';
 import { Panel } from '@enact/sandstone/Panels';
 
-// import CreateButton from '../components/CreateButton';
 import MainTabLayout from '../components/MainTabLayout';
-//import { Route } from '@enact/ui/Routable';
 
 const MainPanel = kind({
 	name: 'MainPanel',
