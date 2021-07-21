7a170cee2a06e0df8d5a9e4481b787dcd3be2178
chavi55 <chavi9116@gmail.com>
2021-07-21 17:26:24 +0900

    camera react code

---
+++ enact/src/views/scan.js
@@ -0,0 +1,8 @@
+import kind from '@enact/core/kind';
+import {Panel, Header} from '@enact/sandstone/Panels';
+import CreateButton from '../components/CreateButton';
+import React from 'react';
+import from
+const 
+
+export default MainPanel;
