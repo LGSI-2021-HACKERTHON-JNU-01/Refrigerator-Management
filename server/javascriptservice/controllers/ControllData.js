d489737e5eaa726bfc3467f9f2247a081bef96aa
ksg0605 <ksg0605@gmail.com>
2021-07-23 15:50:12 +0900

    Add Controller

--- server/javascriptservice/controllers/ControllData.js
+++ server/javascriptservice/controllers/ControllData.js
@@ -1,9 +1,34 @@
 import Data from "../models/Data";
 
-const createData = () => {};
+const createData = async (req, res) => {
+  try {
+    const data = await Data.find();
+    res.status(200).json(data);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+};
 
-const getData = () => {};
+const getData = async (req, res) => {
+  const dataInfo = req.body;
+  try {
+    const newData = new Data(dataInfo);
+    await newData.save();
+    console.log("Saved !");
+    res.status(201).json(newData);
+  } catch (error) {
+    res.status(404).json({ message: error });
+  }
+};
 
-const deleteData = () => {};
+const deleteData = (req, res) => {
+  const { id } = req.params;
+  try {
+    await Data.findByIdAndDelete(id);
+    res.json({ message: "Delete Complete !", result: true });
+  } catch (error) {
+    res.status(404).json({ message: error, result: false });
+  }
+};
 
 export default { createData, getData, deleteData };
