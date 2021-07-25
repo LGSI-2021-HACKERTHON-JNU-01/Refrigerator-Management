67b70439b6bfe8047791ce584aab855a430b9b86
Subinhyun <hkhh972123@gmail.com>
2021-07-26 00:05:27 +0900

    Recipe

---
+++ enact/src/components/RecipeDetails.js
@@ -0,0 +1,22 @@
+import React from "react";
+//import { v4 as uuidv4 } from "react";
+
+const RecipeDetails = ({ title, calories, image, ingredients }) => {
+  
+  return (
+    <div className="ingredient-list">
+        <h1>{title}</h1>
+        <ol className="ingredient-text">
+            {ingredients.map(ingredient =>(
+                <li>{ingredient.text}</li>
+            ))}
+        </ol>
+        <p>{calories}</p>
+        <img className="ingredient-image" src={image} alt=""/>
+    </div>
+)
+  
+  
+};
+
+export default RecipeDetails;
