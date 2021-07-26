ab79b5cb74f406b44e45359fff8bce5fd31c0c70
Subinhyun <hkhh972123@gmail.com>
2021-07-26 12:41:56 +0900

    Recipe

--- enact/src/views/RecipePanel.js
+++ enact/src/views/RecipePanel.js
@@ -1,15 +1,74 @@
-import kind from "@enact/core/kind";
-import { Header, Panel } from "@enact/sandstone/Panels";
+import React, { useState } from "react";
+import "./RecipePanel.css";
+import Axios from "axios";
+import { v4 as uuidv4 } from "react";
+import Recipe from "../components/Recipe";
+import RecipeDetails from "../components/RecipeDetails";
+import Alert from "../components/Alert";
+import Scroller from '@enact/sandstone/Scroller';
 
-const RecipePanel = kind({
-    name: 'Recipe Panel',
+function App() {
+  const [query, setQuery] = useState("");
+  const [recipes, setRecipes] = useState([]);
+  const [alert, setAlert] = useState("");
 
-    render: (props) => (
-        <Panel {...props}>
-            <Header title="Recipe Test Panel" />
-        </Panel>
-    )
+  const APP_ID = "4e9f05eb";
+  const APP_KEY = "9b904d703fa0d46a88ce1ac63f29f498";
 
-});
+  const url = `https://api.edamam.com/search?q=${query}&app_id=${APP_ID}&app_key=${APP_KEY}`;
 
-export default RecipePanel;
+  const getData = async () => {
+    if (query !== "") {
+      let result = await Axios.get(url);
+      if (!result.data.more) {
+        return setAlert("No food with such name");
+      }
+      console.log(result);
+      setRecipes(result.data.hits);
+      setQuery("");
+      setAlert("");
+    } else {
+      setAlert("Please fill the form");
+    }
+  };
+
+  const onChange = e => setQuery(e.target.value);
+
+  const onSubmit = e => {
+    e.preventDefault();
+    getData();
+  };
+
+  return (
+    <Scroller>
+    <div className="App">
+      <h1>Search Recipe</h1>
+      <form onSubmit={onSubmit} className="search-form">
+        {alert !== "" && <Alert alert={alert} />}
+        <input
+          type="text"
+          name="query"
+          onChange={onChange}
+          value={query}
+          autoComplete="off"
+          placeholder="Search Food"
+        />
+        <input type="submit" value="Search" />
+      </form>
+      <div className="recipes">
+      {recipes.map(recipe => (
+            <RecipeDetails
+            key={recipe.recipe.lable}
+            title={recipe.recipe.lable} 
+            calories={recipe.recipe.calories}
+            image = {recipe.recipe.image}
+            ingredients={recipe.recipe.ingredients}
+            />
+            ))}
+      </div>
+    </div>
+    </Scroller>
+  );
+}
+
+export default App;
