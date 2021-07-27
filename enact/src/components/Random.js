7d2acc81ed67cba4958cf8c77c58ed5b73d0822a
Subinhyun <hkhh972123@gmail.com>
2021-07-27 16:18:24 +0900

    recipe

---
+++ enact/src/components/Random.js
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from 'react';
+import Axios from 'axios';
+import Meal from "./Meal";
+const API_URL = "https://www.themealdb.com/api/json/v1/1/random.php";
+
+const RandomMeal = () => {
+     const [meal, setMeal] = useState(undefined);
+
+    useEffect(() => {
+        async function getMeal() {
+            const res = await Axios.get(API_URL);
+            //const data = await res.json();
+            setMeal(res.data.meals[0]);
+        }
+        getMeal();
+    }, [])
+
+    if (!meal) return null;
+
+    return (
+        <section className="container">
+            <Meal meal={meal}/>
+        </section>
+    )
+}
+
+export default RandomMeal;
