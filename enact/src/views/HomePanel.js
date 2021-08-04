58352a0d6f1f0e330fd856031bff347e45830bfa
RohYounwoo <ywroh0619@naver.com>
2021-08-04 15:50:40 +0900

    home update

--- enact/src/views/HomePanel.js
+++ enact/src/views/HomePanel.js
@@ -1,14 +1,29 @@
+
 import kind from "@enact/core/kind";
 import { Header, Panel } from "@enact/sandstone/Panels";
+import Button from '@enact/sandstone/Button';
 
 const HomePanel = kind({
-    name: 'HomePanel',
+  name: 'HomePanel',
 
-    render: (props) => (
-        <Panel {...props}>
-            <Header title="Home Test Panel" />
-        </Panel>
-    )
+  render: (props) => (
+      <Panel {...props}>
+          <Header title="fruit/rotten recognition" />
+          <Button size="small" onClick={(e) => {
+            e.preventDefault();
+            window.location.href='http://localhost:3000/';
+          }}>
+            rotten recognition
+          </Button>
+          <Button size="small" onClick={(e) => {
+            e.preventDefault();
+            window.location.href='http://localhost:7000/fruit.html';
+          }}>
+            fruit recognition
+          </Button>
+          
+      </Panel>
+  )
 
 });
 
